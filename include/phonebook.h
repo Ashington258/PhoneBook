abff9a68f075bce7393890d3c835f8dacb85a63b
小叶同学 <58564210+ThreeSwords66@users.noreply.github.com>
2023-06-13 09:40:33 +0800

    Update phonebook.h

--- include/phonebook.h
+++ include/phonebook.h
@@ -1,10 +1,13 @@
 #ifndef _PHONEBOOK_H
 #define _PHONEBOOK_H
 
-
+/*此函数用户添加用户*/
 void addContact();
+/*此函数用户删除用户*/
 void deleteContact();
+/*此函数用户修改用户*/
 void modifyContact();
+/*此函数用户删除用户*/
 void displayContacts();
 
 
